e05949955add322f75287eab88011bede6aadc2e
Neha Upadhyay <nehau072@gmail.com>
2026-08-17 20:08:16 +0530

    Add protected routing and application components

---
+++ src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navigate, Outlet} from 'react-router-dom';
+import AppLayout from '../layout/AppLayout';
+
+const ProtectedRoute = () => {
+    const isAuthenticated = true;
+    const loading = false;
+    if(loading) {
+        return <div>loading..</div>;
+    }
+   return isAuthenticated ? (
+    <AppLayout>
+        <Outlet/>
+        </AppLayout>
+      ) :(
+    <Navigate to="/login" replace />
+   );
+};
+export default ProtectedRoute;
+    
